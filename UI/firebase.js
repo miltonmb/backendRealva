8adcc2d042756f61ff771fe90d31aa51915db6ed
MarioRaudales23 <marioraudales33@unitec.edu>
2018-06-18 13:26:45 -0600

    firebase works

---
+++ UI/firebase.js
@@ -0,0 +1,17 @@
+var config = {
+    apiKey: "AIzaSyBZAKqZfYiuQDTF-pDtZsxlO5X72wNFA1Q",
+    authDomain: "realva-54c4a.firebaseapp.com",
+    databaseURL: "https://realva-54c4a.firebaseio.com",
+    projectId: "realva-54c4a",
+    storageBucket: "realva-54c4a.appspot.com",
+    messagingSenderId: "233461489484"
+};
+firebase.initializeApp(config);
+function chargeClientes() {
+    let cliente;
+    firebase.database().ref('users/').set({
+        username: "mario",
+        email: "arnulfo",
+    });
+    console.log('entro');
+}
