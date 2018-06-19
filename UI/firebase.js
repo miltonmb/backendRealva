35906190cfce4208898c59ae8831a73ce4cbd28e
MarioRaudales23 <marioraudales33@unitec.edu>
2018-06-18 20:01:29 -0600

    Carga y Paginado listo

--- UI/firebase.js
+++ UI/firebase.js
@@ -1,3 +1,5 @@
+var cliente;
+var paginaactual = 0;
 var config = {
     apiKey: "AIzaSyBZAKqZfYiuQDTF-pDtZsxlO5X72wNFA1Q",
     authDomain: "realva-54c4a.firebaseapp.com",
@@ -7,7 +9,63 @@ var config = {
     messagingSenderId: "233461489484"
 };
 firebase.initializeApp(config);
+window.onload = function () {
+    chargeClientes();
+    updatePaginaActual();
+}
 function chargeClientes() {
-    let cliente;
-    
+    cliente = [];
+    firebase.database().ref('productos').push({ clave: 'luis' });
+    firebase.database().ref('clientes').on('value', function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            cliente.push(childSnapshot.val());
+        });
+        chargeFirstPage();
+    });
+}
+function chargeNextPage() {
+    let prueba = '';
+    let cont = 0;
+    let element;
+    paginaactual++;
+    for (let i = paginaactual * 10; i < cliente.length; i++) {
+        if (cont < 10) {
+            element = cliente[i];
+            prueba += `<tr><td>${element.ClaveCliente}</td><td>${element.NombreEmpresa}</td><td>${element.NombreRepresetante}</td><td>${element.RTN}</td><td>${element.Estatus}</td><td>${element.Telefono1}</td><td>${element.Telefono2}</td><td>${element.Correo}</td><td>${element.Contraseña}</td><td>${element.Clasifiacion}</td><td>${element.Saldo}</td><td>${element.Direccion}</td><td><button>Aceptar</button></td><td><button>Modificar</button></td></tr>`;
+            cont++;
+        }
+    }
+    document.getElementById('clientRows').innerHTML = prueba;
+    updatePaginaActual();
+}
+function chargeFirstPage() {
+    let prueba = '';
+    let cont = 0;
+    cliente.forEach(element => {
+        if (cont < 10) {
+            prueba += `<tr><td>${element.ClaveCliente}</td><td>${element.NombreEmpresa}</td><td>${element.NombreRepresetante}</td><td>${element.RTN}</td><td>${element.Estatus}</td><td>${element.Telefono1}</td><td>${element.Telefono2}</td><td>${element.Correo}</td><td>${element.Contraseña}</td><td>${element.Clasifiacion}</td><td>${element.Saldo}</td><td>${element.Direccion}</td><td><button>Aceptar</button></td><td><button>Modificar</button></td></tr>`;
+            cont++;
+        }
+    });
+    paginaactual = 0;
+    document.getElementById('clientRows').innerHTML = prueba;
+}
+function chargePrevPage() {
+    let prueba = '';
+    let cont = 0;
+    let element;
+    paginaactual--;
+    for (let i = paginaactual * 10; i < cliente.length; i++) {
+        if (cont < 10) {
+            element = cliente[i];
+            prueba += `<tr><td>${element.ClaveCliente}</td><td>${element.NombreEmpresa}</td><td>${element.NombreRepresentante}</td><td>${element.RTN}</td><td>${element.Estatus}</td><td>${element.Telefono1}</td><td>${element.Telefono2}</td><td>${element.Correo}</td><td>${element.Contraseña}</td><td>${element.Clasificacion}</td><td>${element.Saldo}</td><td>${element.Direccion}</td><td><button>Aceptar</button></td><td><button>Modificar</button></td></tr>`;
+            cont++;
+        }
+    }
+    document.getElementById('clientRows').innerHTML = prueba;
+    updatePaginaActual();
+}
+function updatePaginaActual() {
+    prueba = 'Pagina Actual: ' + paginaactual;
+    document.getElementById('htmlPaginaActual').innerHTML = prueba;
 }
