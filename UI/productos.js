8576ee62c69941f591b3e73131805984944b11c2
juanOH7 <juanoh@unitec.edu>
2018-06-26 13:13:30 -0600

    ...
    ...

--- UI/productos.js
+++ UI/productos.js
@@ -22,7 +22,7 @@ function submitOrder() {
     var inContraindication = document.forms["myForm"]["product_contraindication"].value;
     var inCateg = document.forms["myForm"]["product_categ"].value;
     var inTypeUse = document.forms["myForm"]["product_type_use"].value;
-    var inUnidTec = document.forms["myForm"]["product_unidTec_list"].value;
+    var inUnidTec = document.forms["myForm"]["product_unidTec"].value;
     var inSpecies = document.forms["myForm"]["product_species_list"].value;
     var inPrice = document.forms["myForm"]["product_price"].value;
     /*if (inCode == "") {
