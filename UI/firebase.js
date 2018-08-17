63613e6efca572b460a5fcb16f1675c24e94daa8
MarioRaudales23 <marioraudales33@unitec.edu>
2018-08-17 12:07:31 -0600

    buscar cliente

--- UI/firebase.js
+++ UI/firebase.js
@@ -143,4 +143,41 @@ function onModificar(id) {
     document.getElementById('Estatus').value = 'Activo';
     chargeClientes(1);
 }
-
+function buscarCliente(elemento){
+    let element = null;
+    for (let i = paginaactual * 10; i < cliente.length; i++) {
+        if (cliente[i].id === elemento) {
+            element = cliente[i];
+        }
+        if (cliente[i].NombreEmpresa === elemento) {
+            element = cliente[i];
+        }
+        if (cliente[i].NombreRepresentante === elemento) {
+            element = cliente[i];
+        }
+        if (cliente[i].RTN === elemento) {
+            element = cliente[i];
+        } 
+    }
+    if(element !=null){
+        tempcliente = element;
+        document.getElementById('ClaveCliente').value = element.ClaveCliente;
+        document.getElementById('Clasificacion').value = element.Clasificacion;
+        document.getElementById('Correo').value = element.Correo;
+        document.getElementById('Direccion').value = element.Direccion;
+        document.getElementById('Estatus').value = element.Estatus;
+        document.getElementById('NombreEmpresa').value = element.NombreEmpresa;
+        document.getElementById('NombreRepresentante').value = element.NombreRepresentante;
+        document.getElementById('RTN').value = element.RTN;
+        document.getElementById('Saldo').value = element.Saldo;
+        document.getElementById('Telefono1').value = element.Telefono1;
+        document.getElementById('Telefono2').value = element.Telefono2;
+        document.getElementById('id01').style.display='block';
+    }else{
+        document.getElementById('id03').style.display='block';
+    }
+}
+function buscar(){
+    var textobuscar = document.getElementById('busqueda').value;
+    buscarCliente(textobuscar);
+}
