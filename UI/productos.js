39171b0fc4d75dabe503cdfbfedc8476a60bfd0d
Unknown <juanoh@unitec.edu>
2018-08-30 16:41:01 -0600

    producto 1.0
    más completo.

--- UI/productos.js
+++ UI/productos.js
@@ -17,7 +17,7 @@ function submitOrder() {
     //Grab order data from the form
     var inCode = document.forms["myForm"]["product_code"].value;
     var inName = document.forms["myForm"]["product_name"].value;
-    var inDosis = document.forms["myForm"]["product_dosis"].value;
+    // var inDosis = document.forms["myForm"]["product_dosis"].value;
     var inDescription = document.forms["myForm"]["product_description"].value;
     var inIndications = document.forms["myForm"]["product_indications"].value;
     var inContraindication = document.forms["myForm"]["product_contraindication"].value;
@@ -25,7 +25,7 @@ function submitOrder() {
     var inTypeUse = $('#typeUse').val();
     var inUnidTec = $('#product_unidTec').val();
     var inSpecies = $('#product_species_list').val();
-    var inPrice = document.forms["myForm"]["product_price"].value;
+    //  var inPrice = document.forms["myForm"]["product_price"].value;
     var inLab = $('#product_labo').val();
 
     /*if (inCode == "") {
@@ -75,6 +75,20 @@ function submitOrder() {
     if (selectedfile == null) {
 
     } else {
+
+        var oTable = document.getElementById('tablaDosis');
+        var rowLength = oTable.rows.length;
+        var dosisList = [];
+      //  var precioList = [];
+
+        for (i = 1; i < rowLength; i++) {
+            var cellVal = $('#dosis' + i).val();
+            var cellVal2 = $('#product_price' + i).val();
+            let temp = {dosis:cellVal,precio:cellVal2}
+            dosisList.push(temp);
+        //    precioList.push(cellVal2);
+        }
+
         const newProduct = firebase.database().ref('/productos').push();
         newProduct.set({
             imagen: selectedfile,
@@ -84,6 +98,8 @@ function submitOrder() {
             indicacion: inIndications,
             contraindicacion: inContraindication,
             //dosis: inDosis,
+            presentacion: dosisList,
+          //  precio:precioList,
             categ: inCateg,
             tipoUso: inTypeUse,
             especie: inSpecies,
@@ -93,6 +109,7 @@ function submitOrder() {
             visita: 0
         });
         alert("Se agrego con éxito!");
+        cerrarModal();
     }
     //'products' is the name of the 'collection' (aka database table)
     document.getElementById("btnSubir").reset();
@@ -175,46 +192,68 @@ function removeRow(that) {
 }
 
 function cargarDatos() {
-    console.log("cargarDatos");
     var inCode = document.forms["myForm"]["product_code"].value;
     var inName = document.forms["myForm"]["product_name"].value;
-    var inDosis = document.forms["myForm"]["product_dosis"].value;
-    console.log(inDosis);
+    //    var inDosis = document.forms["myForm"]["product_dosis"].value;
     var inDescription = document.forms["myForm"]["product_description"].value;
     var inIndications = document.forms["myForm"]["product_indications"].value;
     var inContraindication = document.forms["myForm"]["product_contraindication"].value;
     var inCateg = $('#product_categ').val();
     var inTypeUse = $('#typeUse').val();
-    console.log(inTypeUse);
     var inUnidTec = $('#product_unidTec').val();
     var inSpecies = $('#product_species_list').val();
-    var inPrice = document.forms["myForm"]["product_price"].value;
+    //  var inPrice = document.forms["myForm"]["product_price"].value;
     var inLab = $('#product_labo').val();
 
-    var files = document.getElementById('imagenProducto').files;
-    var reader = new FileReader();
-    reader.readAsDataURL(files[0]);
-    reader.onload = function () {
-        selectedfile = reader.result;
-        document.getElementById('mostrarImg').innerHTML = `
-        <img id="img01" src='${reader.result}' class='imagenesDisplay' style="width:100%;max-width:800px">`
-    };
+    /*    var files = document.getElementById('imagenProducto').files;
+        var reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+        reader.onload = function () {
+            selectedfile = reader.result;
+            document.getElementById('mostrarImg').innerHTML = `
+            <img id="img01" src='${reader.result}' class='imagenesDisplay' style="width:100%;max-width:800px">`
+        };*/
 
     document.getElementById('code').value = inCode;
     document.getElementById('name').value = inName;
     document.getElementById('description').value = inDescription;
     document.getElementById('indications').value = inIndications;
     document.getElementById('contraindication').value = inContraindication;
-    document.getElementById('dosisModal').value = inDosis;
+    // document.getElementById('dosisModal').value = inDosis;
     document.getElementById('categ').value = inCateg;
     document.getElementById('typeUseModal').value = inTypeUse;
     document.getElementById('unidTec').value = inUnidTec;
     document.getElementById('species').value = inSpecies;
-    document.getElementById('price').value = inPrice;
+    // document.getElementById('price').value = inPrice;
     document.getElementById('lab').value = inLab;
+    let newRow = '';
+    var oTable = document.getElementById('tablaDosis');
+    var rowLength = oTable.rows.length;
+    /*
+    <tr>
+    <th>Presentación(Dosis)</th>
+    <th>Precio</th>
+    </tr>
+    */
 
+    newRow += `<tr>
+    <th>Presentación(Dosis)</th>
+    <th>Precio</th>
+    </tr>`;
+    for (i = 1; i < rowLength; i++) {
+        var cellVal = $('#dosis' + i).val();
+        var cellVal2 = $('#product_price' + i).val();
+        newRow += `<tr><td>${cellVal}</td><td>${cellVal2}</td></tr>`;
+    }
+    document.getElementById('tablaDos').innerHTML = newRow;
 }
 
+
+function cerrarModal() {
+    document.getElementById('id01').style.display = 'none';
+    var new_tbody = document.createElement('tbody');
+    $('#body1').replaceWith(new_tbody);
+}
 
 function getTableData() {
     //table
@@ -232,12 +271,12 @@ function getTableData() {
         var cellLength = oCells.length - 1;
 
         //loops through each cell in current row
-            var cellVal = $('#dosis' + i).val();
-            var cellVal2 = $('#product_price' + i).val();
+        var cellVal = $('#dosis' + i).val();
+        var cellVal2 = $('#product_price' + i).val();
 
-            //oCells.item(j).value;
-            console.log(cellVal);
-            console.log(cellVal2);
+        //oCells.item(j).value;
+        console.log(cellVal);
+        console.log(cellVal2);
 
     }
 }
